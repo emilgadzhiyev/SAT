510762bdb00d42d5f5ae9f55354e8fd2b461373a
Emil Gadzhiyev <emkamakeoff@gmail.com>
2020-11-13 01:29:27 +0400

    added animation to layout

---
+++ src/js/OffCanvasMenu.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const aside = document.querySelector('.layout__left');
+
+hamburger.addEventListener('click', function (e) {
+    aside.classList.toggle('layout__left_hidden');
+});
+
